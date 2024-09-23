8ffcf9eb2a545b18ab53fba9d8de7ccbd02e8807
Chris Careaga <chris_careaga@sfu.ca>
2024-09-22 20:29:59 -0700

    Updating decompose.py script

--- intrinsic/decompose.py
+++ intrinsic/decompose.py
@@ -1,29 +1,23 @@
-from chrislib.general import view, tile_imgs, view_scale, uninvert
+from chrislib.general import view, tile_imgs, invert
 from chrislib.data_util import load_image
 
-from intrinsic.pipeline import run_pipeline
-from intrinsic.model_util import load_models
+from intrinsic.pipeline import load_models, run_pipeline
 
 # load the models from the given paths
-models = load_models('paper_weights')
+models = load_models('v2')
 
 # load an image (np float array in [0-1])
 image = load_image('/path/to/input/image')
 
-# run the model on the image using R_0 resizing 
-# the maintain_size will output components that match the original image dims
-results = run_pipeline(
-    models,
-    image,
-    resize_conf=0.0,
-    maintain_size=True
-)
+# run the model on the image (will use R0 resizing used in the ordinal shading paper by default)
+results = run_pipeline(models, image, device='cuda')
 
-albedo = results['albedo']
-inv_shd = results['inv_shading']
-
-# compute shading from inverse shading
-our_shd = uninvert(inv_shd)
+img = results['image']
+alb = view(results['hr_alb']) # gamma correct the estimated albedo
+dif = 1 - invert(results['dif_shd']) # tonemap the diffuse shading
+res = results['residual']
 
 # save intrinsic components scaled to [0, 1] and gamma corrected
-tile_imgs([image, view(albedo), view(our_shd)], save='output.png')
+tile_imgs([img, alb, dif, res], save='output.png')
+
+
